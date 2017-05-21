b19af026fb26a8659fa1fa4aa5c149f2e15d671e
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-05-20 23:20:30 -0600

    added music

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
 // Variables
-	// Letters to guess
-	var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+
+	// Music Variables
+	// Gets Link for Theme Song
+      var rocketeer = new Audio("assets/music/rocketeer.mp3");
+
+      var baldursGate = new Audio("assets/music/BaldursGate.mp3");
+
+      var kq6 = new Audio("assets/music/KQ6.mp3");
+
+      var legendofZelda = new Audio("assets/music/LegendofZelda.mp3");
 
 	// Word bank
 	var wordsToGuess = ["rocketeer", "kingsquest", "legendofzelda", "baldursgate"];
@@ -18,12 +25,14 @@ document.addEventListener("DOMContentLoaded", function() {
 	// number of guesses remaining
 	var guesses = 10;
 
-	// Letter already guessed
-	var guessedLetters = [];
-
 	// User Guess
 	var userGuess = event.key;
 
+	var remainingGuesses  = document.getElementById("remainingGuesses");
+
+	var guessedLetters = document.getElementById("guessedletters");
+
+	var numberofWins = document.getElementById("wins");
 	// Setting up random word to guess
 	var currentWord = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
 
@@ -37,36 +46,90 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	var remainingLetters = currentWord.length;
 
-	// Showing Remaining Guesses
-	remainingGuesses.innerHTML = guesses;
+		remainingGuesses.innerHTML = guesses;
+		numberofWins.innerHTML = wins;
+
+		function checkGuess (key) {
+				var badGuess = true;
+				for (var j = 0; j < currentWord.length; j++) {
+					if (currentWord[j] === key) {
+						console.log("Letters remaining " + remainingLetters, key, currentWord[j]);
+						answerArray[j] = key;
+						badGuess = false  						// remainingLetters--;
+					} 
+				}
+
+					// Join connects an array
+					if (answerArray.join("") === currentWord) {
+						winCounter();
+						musicPlay();
+					}
+			
+					if (badGuess) {
+							// Showing Remaining Guesses
+							guesses--; 
+							remainingGuesses.innerHTML = guesses;
+							// Note: Shows up as undefined in browser.
+							wrongGuess();
+						}
+
+					// if (answerArray.join("") === currentWord && badGuess = true) {
+					// }
+
+					// Updating the HTML
+					document.querySelector("#currentWord").innerHTML = answerArray;
+		}
+
+// function statement - this function exists to call a function () accesses the functions actions
+		function winCounter () {
+				wins++; 
+				document.getElementById("wins").innerHTML = wins;
+			
+		}
+
+		function wrongGuess () {
+			guessedLetters.innerHTML = userGuess;
+		}
+
+		
+		// Game resets if guesses run out.
+		function outOfGuesses () {
+
+		}
+
+		// Plays music when the correct word is guessed
+		function musicPlay () {
+
+			if (currentWord === "rocketeer") {
+				rocketeer.play();
+			} else if (currentWord === "legendofzelda") {
+				legendofZelda.play();
+			} else if (currentWord === "kingsquest") {
+				kq6.play();
+			} else if (currentWord === "baldursgate") {
+				baldursGate.play();
+			} else {
+				audio.muted = true;
+			}
+		}
+
+		function switchPicture () {
+
+			if (currentWord === "rocketeer") {
+				rocketeer.play(); 
+			}
+		}
+
+  
+		// Play winning music and show picture.
+		// Change the word to guess.
+
 
 		document.onkeyup = function(event) {
 		// inside key up
 		console.log("Inside Key Up!");
-		console.log("Issue with while block");
-		console.log("Letters remaining " + remainingLetters);
-			
 
-		userGuess = event.key;
-			// Use if instead of while
-			if (remainingLetters > 0) {
-				for (var k = 0; k < alphabet.length; k++) {
-					if (alphabet[k] === userGuess) {
-						console.log ("What user guesses " + userGuess);
-					for (var j = 0; j < currentWord.length; j++) {
-					if (currentWord[j] === userGuess) {
-						answerArray[j] = userGuess;
-						remainingLetters--;
-					}
-				}
-
-			}
-
-
-					// Updating the HTML
-					document.querySelector("#currentWord").innerHTML = answerArray;
-				}
-			}
+		 checkGuess(event.key);
 
 		}
 });
